35f5d79afde749e3f962677f3a0a3a5a8adf3146
David Kerber <dave@kerber.io>
2024-12-07 17:44:49 -0600

    Adding a new export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export type { Action, ActionType, ServiceAction, WildcardAction } from './actions/action.js'
 export type { Condition } from './conditions/condition.js'
 export type { ConditionOperation, SetOperator } from './conditions/conditionOperation.js'
-export { loadPolicy } from './parser.js'
+export { loadAnnotatedPolicy, loadPolicy } from './parser.js'
 export type { Policy } from './policies/policy.js'
 export type { AccountPrincipal, AwsPrincipal, CanonicalUserPrincipal, FederatedPrincipal, Principal, PrincipalType, ServicePrincipal, WildcardPrincipal } from './principals/principal.js'
 export type { Resource } from './resources/resource.js'
