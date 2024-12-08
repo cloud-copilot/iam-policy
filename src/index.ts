7f7ad8ea46dd2350c3cbd80067891810b9d2d11b
David Kerber <dave@kerber.io>
2024-12-07 18:36:33 -0600

    Adding exports and prettier dependency

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-export type { Action, ActionType, ServiceAction, WildcardAction } from './actions/action.js'
-export type { Condition } from './conditions/condition.js'
+export type { Action, ActionType, AnnotatedAction, ServiceAction, WildcardAction } from './actions/action.js'
+export type { AnnotatedCondition, Condition } from './conditions/condition.js'
 export type { ConditionOperation, SetOperator } from './conditions/conditionOperation.js'
 export { loadAnnotatedPolicy, loadPolicy } from './parser.js'
-export type { Policy } from './policies/policy.js'
-export type { AccountPrincipal, AwsPrincipal, CanonicalUserPrincipal, FederatedPrincipal, Principal, PrincipalType, ServicePrincipal, WildcardPrincipal } from './principals/principal.js'
-export type { Resource } from './resources/resource.js'
-export type { ActionStatement, NotActionStatement, NotPrincipalStatement, NotResourceStatement, PrincipalStatement, ResourceStatement, Statement } from './statements/statement.js'
+export type { AnnotatedPolicy, Policy } from './policies/policy.js'
+export type { AccountPrincipal, AnnotatedPrincipal, AwsPrincipal, CanonicalUserPrincipal, FederatedPrincipal, Principal, PrincipalType, ServicePrincipal, WildcardPrincipal } from './principals/principal.js'
+export type { AnnotatedResource, Resource } from './resources/resource.js'
+export type { ActionStatement, AnnotatedActionStatement, AnnotatedNotActionStatement, AnnotatedNotPrincipalStatement, AnnotatedNotResourceStatement, AnnotatedPrincipalStatement, AnnotatedResourceStatement, AnnotatedStatement, NotActionStatement, NotPrincipalStatement, NotResourceStatement, PrincipalStatement, ResourceStatement, Statement } from './statements/statement.js'
 export { validatePolicySyntax, type ValidationError } from './validate/validate.js'
 export {
   validateEndpointPolicy,
