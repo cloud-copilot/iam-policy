9ab9d129f784fb9d8f635c71e360fff5e3838a02
David Kerber <dave@kerber.io>
2025-10-04 11:48:25 -0500

    feat: Add toJSON to Statement

--- src/statements/statement.ts
+++ src/statements/statement.ts
@@ -82,6 +82,11 @@ export interface Statement {
    * The path to the statement in the policy
    */
   path(): string
+
+  /**
+   * The raw policy object as JSON
+   */
+  toJSON(): any
 }
 
 /**
@@ -310,6 +315,10 @@ export class StatementImpl
       )
     }
     return this.statementObject.NotPrincipal === '*'
+  }
+
+  public toJSON(): any {
+    return this.statementObject
   }
 
   /**
